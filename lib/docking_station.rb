924cc67a9fb41e5fae6b7be7b9aeac708797133d
John Newman <newmanj903@gmail.com>
2018-04-19 12:42:32 +0100

    exercise 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,20 @@
 require_relative 'bike'
 
 class DockingStation
-attr_reader :bike
+  attr_reader :bikes
 
-  def release_bike
-    raise "No bikes available" if @bike == nil
-    @bike
+  def initialize
+    @bikes = []
   end
 
-  def dock(bike)
-    raise "Docking Station full" if @bike
-    @bike = bike
+  def release_bike
+    raise "No bikes available" if @bikes == []
+    @bikes.pop
+  end
+
+  def dock(bicycle)
+    raise "Docking Station full" if @bikes.length == 20
+    @bikes.push(bicycle)
   end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,12 +1,17 @@
 require 'docking_station'
 
 describe DockingStation do
+  it 'has a bike method' do
+      is_expected.to respond_to(:bikes)
+  end
 
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
+      expect(subject.bikes).to eq []
+
     end
     it 'raises an error when no bikes available' do
       expect { subject.release_bike }.to raise_error "No bikes available"
@@ -15,8 +20,13 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when docking station is full' do
-      subject.dock Bike.new
-      expect { subject.dock Bike.new }.to raise_error "Docking Station full"
+      20.times {subject.dock Bike.new} # No need to declare bike instance seprately bike = Bike.new
+      expect { subject.dock Bike.new }.to raise_error "Docking Station full" #bike2 = Bike.new
+    end
+    it 'returns bike when its docked' do
+      bicycle = Bike.new
+      subject.dock(bicycle)
+      expect(subject.bikes).to eq [bicycle]
     end
   end
 end

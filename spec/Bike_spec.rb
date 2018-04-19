394d67707bdf1edb34460e8d95705274f40bbb94
John Newman <newmanj903@gmail.com>
2018-04-19 15:16:16 +0100

    bike report broken

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -1,5 +1,14 @@
 require 'bike'
 
 describe Bike do
-  it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :working }
+  it { is_expected.to respond_to :report_broken }
+
+  describe '#report_broken' do
+    it "should allow the user to report a bike is broken" do
+      subject.report_broken
+      expect(subject.working).to eq false
+    end
+
+  end
 end
